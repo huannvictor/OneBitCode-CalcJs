3e04c56a57fcc4cb46da527d7575c1292746c911
Huann Almeida <huannvictor@gmail.com>
2022-11-10 11:12:53 -0300

    💅 light theme color changed to #fff

--- modules/themeSwitcher.js
+++ modules/themeSwitcher.js
@@ -2,7 +2,7 @@ import { main, root } from "./declarations.js";
 
 export function themeSwitcher() {
   if (main.dataset.theme === "dark") {
-    root.style.setProperty("--bg-color", "#ccc");
+    root.style.setProperty("--bg-color", "#fff");
     root.style.setProperty("--border-color", "#aaa");
     root.style.setProperty("--font-color", "#333");
     root.style.setProperty("--primary-color", "#1db055");
@@ -11,7 +11,7 @@ export function themeSwitcher() {
   } else {
     root.style.setProperty("--bg-color", "#333");
     root.style.setProperty("--border-color", "#666");
-    root.style.setProperty("--font-color", "#ccc");
+    root.style.setProperty("--font-color", "#fff");
     root.style.setProperty("--primary-color", "#4dff91");
 
     main.dataset.theme = "dark";
